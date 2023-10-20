db1a02f6923970d046d5556f3e787c24a25028fd
developersharif <developersharif@yahoo.com>
2023-10-20 21:59:53 +0600

    index component added

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,18 +1,17 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
+import Index from './Todo/Index';
 
 export default function Dashboard({ auth ,todos}) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Todos</h2>}
+
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        {(todos.length ==0) ? (<div><p>No todos available</p></div>) :
+
+                        {/* {(todos.length ==0) ? (<div><p>No todos available</p></div>) :
                        ( <ul>
                         {todos && todos.map((todo) => (
                             <div className='flex justify-between p-5' key={todo.id}>
@@ -27,11 +26,10 @@ export default function Dashboard({ auth ,todos}) {
         </span>
                             </div>
       ))}
-                        </ul>)}
+                        </ul>)} */}
+                        <Index todos={todos} auth={auth} />
 
-                    </div>
-                </div>
-            </div>
+
         </AuthenticatedLayout>
     );
 }
