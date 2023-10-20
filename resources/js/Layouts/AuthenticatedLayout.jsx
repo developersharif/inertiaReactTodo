ae9590b49841c738d87831b419e0fede59d42a1f
developersharif <developersharif@yahoo.com>
2023-10-20 11:29:56 +0600

    route and flash messages added

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Link } from '@inertiajs/react';
-
+import { Link,usePage } from '@inertiajs/react';
+import { ReactNotifications } from 'react-notifications-component'
+import { Store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
+import 'animate.css/animate.min.css';
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-
+    const { flash } = usePage().props
+    useEffect(() => {
+        if (flash.message) {
+          const notification = {
+            title: flash.type,
+            message: flash.message,
+            type: flash.type,
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeInRight"],
+            animationOut: ["animate__animated", "animate__zoomOut"],
+            dismiss: {
+              duration: 3000,
+              onScreen: true,
+            },
+          };
+          Store.addNotification(notification);
+        }
+      }, [flash]);
     return (
         <div className="min-h-screen bg-gray-100">
+            <ReactNotifications />
             <nav className="bg-white border-b border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
@@ -22,7 +44,10 @@ export default function Authenticated({ user, header, children }) {
 
                             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
+                                    Todo
+                                </NavLink>
+                                <NavLink href={route('todo.create')} active={route().current('todo.create')}>
+                                    Create
                                 </NavLink>
                             </div>
                         </div>
@@ -118,8 +143,9 @@ export default function Authenticated({ user, header, children }) {
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
                 </header>
             )}
-
-            <main>{children}</main>
+            <main>
+                {children}
+                </main>
         </div>
     );
 }
